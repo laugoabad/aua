be5d9643b26b80f0a14ec2e571a1c980f13335b1
laugoabad <36644178+laugoabad@users.noreply.github.com>
2020-07-09 18:21:34 -0300

    k

--- src/components/SubmissionSuccessful.vue
+++ src/components/SubmissionSuccessful.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="padd-top">
-    <h2 class="mt-4 p-4 w-75 ml-auto mr-auto">Gracias por tu contacto! Te contestamos lo antes posible!</h2>
+    <h2 class="mt-4 p-4 w-75 ml-auto mr-auto">Gracias por tu contacto! Te contestaremos lo antes posible!</h2>
     <router-link to="/">
      <button class="btn btn-default mt-4 px-4 py-2 texto-chico d-flex ">
         Volver
