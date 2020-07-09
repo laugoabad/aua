514fdf0d41a8ad6174b3c1ba20afbfa558f5420b
laugoabad <36644178+laugoabad@users.noreply.github.com>
2020-07-09 15:13:58 -0300

    s

--- src/components/SubmissionSuccessful.vue
+++ src/components/SubmissionSuccessful.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 class="padd-top">You're Awesome! Thank you for your submission!</h1>
+  <div class="padd-top">
+    <h1>You're Awesome! Thank you for your submission!</h1>
     <router-link to="/">
       <button>Back to form</button>
     </router-link>
