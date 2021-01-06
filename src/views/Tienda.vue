bed9b971125add1b370fdb83cedf6d3bd6bbc517
laugoabad <36644178+laugoabad@users.noreply.github.com>
2021-01-06 09:55:15 -0300

    k

--- src/views/Tienda.vue
+++ src/views/Tienda.vue
@@ -91,6 +91,8 @@
                       </div>
                     </div>
                   </div>
+
+                  <!-- -----modal end---- -->
                </div>
               </div>
             </div>
