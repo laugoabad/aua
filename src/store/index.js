a7c4de2c99ac86fbef6c269e00bdf252f03d3195
laugoabad <36644178+laugoabad@users.noreply.github.com>
2021-06-30 08:36:44 -0300

    change prices

--- src/store/index.js
+++ src/store/index.js
@@ -11,13 +11,13 @@ export default new Vuex.Store({
         imagenes: ["manta-1-5.jpg", "manta-1-3.jpg", "manta-1-4.jpg", "manta-1-6.jpg", "manta-1-1.png", "manta-1-7.jpg", "manta-1-8.jpg"],
         descrCorta: "Manta pura lana",
         descrLarga: "Manta tejida a mano en pura lana, suave y abrigada.\nMide 100 x 180cm y viene en color crudo, gris, visón claro y chocolate.\nLas hacemos en distintos puntos y en general en pares para usar en cama doble",
-        precio: 6900
+        precio: 9500
       },
       {
         imagenes: ["manta-2-2.png", "manta-2-1.jpg", "manta-2-3.jpg"],
         descrCorta: "Manta pura lana baby Merino",
         descrLarga: "Manta tejida a mano en pura lana baby Merino, súper suave, esponjosa y abrigada.\nMide 100 x 180cm y viene sólo en color crudo",
-        precio: 8900
+        precio: 9500
       },
       {
         imagenes: ["tote-1.jpeg", "tote-2.jpeg", "tote-3.jpeg", "tote-4.jpeg"],
@@ -38,7 +38,7 @@ export default new Vuex.Store({
         descrCorta: "Almohadón en vellón de lana",
         descrLarga:
           "Almohadón en pura lana, cómodo y calentito es ideal para agregar a las sillas de plástico.",
-        precio: 2200
+        precio: 2500
       },
     ]
   },
