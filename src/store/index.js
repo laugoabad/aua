9ccab78d06958160f6d079fadc3dd33d2d5a6ded
laugoabad <36644178+laugoabad@users.noreply.github.com>
2023-04-21 17:59:43 -0300

    primer comit de nueva pagina

--- src/store/index.js
+++ src/store/index.js
@@ -15,8 +15,8 @@ export default new Vuex.Store({
         ],
         descrCorta: "Manta pura lana",
         descrLarga:
-          "Tejidas a mano en pura lana merino fina, muy suave. Mantienen tu calor sin agobiar y dejando respirar al cuerpo.\nEn stock color crudo.",
-        precio: 9500,
+          "Tejidas a mano en pura lana merino fina, muy suave. Mantienen tu calor sin agobiar y dejando respirar al cuerpo.\nEn stock color melange vison, melange chocolate y gris.",
+        precio: 28000,
       },
       {
         imagenes: [
@@ -40,33 +40,33 @@ export default new Vuex.Store({
           "Manta tejida a mano en pura lana baby Merino, súper suave, esponjosa y abrigada.\nMide 100 x 180cm y viene sólo en color crudo",
         precio: 9500,
       },
-      {
-        imagenes: ["tote-1.jpeg", "tote-2.jpeg", "tote-3.jpeg", "tote-4.jpeg"],
-        descrCorta: "Tote-Bag en hilo de algodón",
-        descrLarga:
-          "Bolso tejido a mano en algodón color gris.\nEntran muchísmas cosas y casi no ocupa espacio vacío. \nViene en varios colores",
-        precio: 700,
-      },
-      {
-        imagenes: [
-          "bolso-pera-1.png",
-          "bolso-pera-2.png",
-          "bolso-pera-4.png",
-          "bolso-pera-5.png",
-        ],
-        descrCorta: "Bolso bandolera",
-        descrLarga:
-          "Bolso tejido a mano súper práctico y canchero. \nViene en varias combinaciones de colores y también se puede hacer con la correa más corta.",
-        precio: 750,
-      },
+      // {
+      //   imagenes: ["tote-1.jpeg", "tote-2.jpeg", "tote-3.jpeg", "tote-4.jpeg"],
+      //   descrCorta: "Tote-Bag en hilo de algodón",
+      //   descrLarga:
+      //     "Bolso tejido a mano en algodón color gris.\nEntran muchísmas cosas y casi no ocupa espacio vacío. \nViene en varios colores",
+      //   precio: 700,
+      // },
+      // {
+      //   imagenes: [
+      //     "bolso-pera-1.png",
+      //     "bolso-pera-2.png",
+      //     "bolso-pera-4.png",
+      //     "bolso-pera-5.png",
+      //   ],
+      //   descrCorta: "Bolso bandolera",
+      //   descrLarga:
+      //     "Bolso tejido a mano súper práctico y canchero. \nViene en varias combinaciones de colores y también se puede hacer con la correa más corta.",
+      //   precio: 750,
+      // },
 
-      {
-        imagenes: ["almohadon-1.png", "almohadon-2.png", "almohadon-3.png"],
-        descrCorta: "Almohadón en vellón de lana",
-        descrLarga:
-          "Almohadón en pura lana, cómodo y calentito es ideal para agregar a las sillas de plástico.",
-        precio: 2500,
-      },
+      // {
+      //   imagenes: ["almohadon-1.png", "almohadon-2.png", "almohadon-3.png"],
+      //   descrCorta: "Almohadón en vellón de lana",
+      //   descrLarga:
+      //     "Almohadón en pura lana, cómodo y calentito es ideal para agregar a las sillas de plástico.",
+      //   precio: 2500,
+      // },
     ],
   },
   mutations: {},
